b722aea26504f26d2be8ad4badf4f57d8ab8a600
DesenbergDienstleistungsGbR <ehancken@t-online.de>
2026-01-12 03:54:17 +0100

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@
 // ist aber für GitHub Pages (statische Website) die praktikable Variante.
 const AUTH_KEY = "schlagkartei_authed_v1";
 // Ändere hier dein Passwort:
-const PASSWORD = "passwort";
+const PASSWORD = "Geheim";
 
 export function isAuthed() {
   return localStorage.getItem(AUTH_KEY) === "1";
